76d95a34d8a66aaab22f36cdc2e022d277dafa18
gael55x <gailleamolong5@gmail.com>
2023-09-25 22:29:21 +0800

    added chatbot.py and updated requirements.txt -> having problems with accuracy

--- chatbot.py
+++ chatbot.py
@@ -4,8 +4,10 @@ import time
 import os
 import math 
 import string 
+import spacy
 
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -13,6 +15,9 @@ SENTENCE_MATCHES = 1
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+
+# Load the spaCy NER model
+nlp = spacy.load("en_core_web_sm")
 
 # Initialize the lemmatizer
 lemmatizer = WordNetLemmatizer()
@@ -31,22 +36,27 @@ def print_with_appearance(text):
 """ Main program """
 def main():
 
-        # Check command-line arguments
+    # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python chatbot.py corpus")
 
     # Calculate IDF values across files
     files = load_files(sys.argv[1])
     file_words = {
-        filename: tokenize(files[filename])
+        filename: tokenize(files[filename], set())  # Pass an empty set for named_entities
         for filename in files
     }
 
+    # Perform NER on the documents and get named entities
+    named_entities = set()
+    for filename in files:
+        named_entities.update(perform_ner(files[filename]))
+
     # Prompt user for query
-    query = set(tokenize(input("Query: ")))
+    query = set(tokenize(input("Query: "), named_entities))
 
     # Calculate IDF values for words based on the query (with attention)
-    file_idfs = compute_idfs(file_words, query)
+    file_idfs = compute_idfs(file_words, query, named_entities)
 
     # Determine top file matches according to TF-IDF
     filenames = top_files(query, file_words, file_idfs, n=FILE_MATCHES)
@@ -56,15 +66,15 @@ def main():
     for filename in filenames:
         for passage in files[filename].split("\n"):
             for sentence in nltk.sent_tokenize(passage):
-                tokens = tokenize(sentence)
+                tokens = tokenize(sentence, named_entities)  # Pass named_entities here
                 if tokens:
                     sentences[sentence] = tokens
 
     # Compute IDF values for sentences based on the query (with attention)
-    idfs = compute_idfs(sentences, query)
+    idfs = compute_idfs(sentences, query, named_entities)
 
     # Determine top sentence matches
-    matches = top_sentences(query, sentences, idfs, n=SENTENCE_MATCHES)
+    matches = top_sentences(query, sentences, idfs, named_entities, n=SENTENCE_MATCHES)
     for match in matches:
         # Concatenate the matches into a single string
         matches_str = ' '.join(matches)
@@ -90,8 +100,16 @@ def load_files(directory):
 
     return file_dict
 
+def perform_ner(document):
+    """
+    Given a document (string), perform Named Entity Recognition (NER) using spaCy.
+    Returns a list of named entities found in the document.
+    """
+    doc = nlp(document)
+    entities = [ent.text for ent in doc.ents]
+    return entities
 
-def tokenize(document):
+def tokenize(document, named_entities):
     """
     Given a document (represented as a string), return a list of all of the
     words in that document, in order.
@@ -107,7 +125,7 @@ def tokenize(document):
 
     # Ensure all tokens are lowercase, non-stopwords, non-punctuation
     for token in tokens:
-        if token in nltk.corpus.stopwords.words('english'):
+        if token in nltk.corpus.stopwords.words('english') or token in string.punctuation or token in named_entities:
             continue
 
         else:
@@ -124,7 +142,7 @@ def tokenize(document):
     return cleaned_tokens
 
 
-def compute_idfs(documents, query):
+def compute_idfs(documents, query, named_entities):
     """
     Given a dictionary of `documents` that maps names of documents to a list
     of words, return a dictionary that maps words to their IDF values.
@@ -133,30 +151,31 @@ def compute_idfs(documents, query):
     resulting dictionary. The IDF values will be influenced by the query.
     """
 
-    # Number of documents for idf:
-    num_docs = len(documents)
+    # Join the tokenized words into strings for each document
+    document_strings = [" ".join(words) for words in documents.values()]
 
-    # Dictionary to count number of docs containing each word:
-    docs_with_word = dict()
+    # Create a TfidfVectorizer instance
+    tfidf_vectorizer = TfidfVectorizer()
 
-    # Iterate through documents looking at unique words in each:
-    for document in documents:
-        doc_words = set(documents[document])
+    # Fit the vectorizer to the document strings
+    tfidf_matrix = tfidf_vectorizer.fit_transform(document_strings)
 
-        for word in doc_words:
-            if word not in docs_with_word:
-                docs_with_word[word] = 1
-            else:
-                docs_with_word[word] += 1
+    # Get the feature names from the vectorizer
+    feature_names = tfidf_vectorizer.get_feature_names_out()
 
-    # Calculate idfs for each word:
-    word_idfs = dict()
-    for word in docs_with_word:
-        word_idfs[word] = math.log((num_docs / docs_with_word[word])) * query_attention(word, query)
+    # Get the IDF values from the vectorizer
+    idf_values = tfidf_vectorizer.idf_
+
+    # Create a mapping of words to IDF values
+    word_idfs = {word: idf for word, idf in zip(feature_names, idf_values)}
+
+    # Adjust IDF values based on query attention
+    for word in word_idfs:
+        word_idfs[word] *= query_attention(word, query, named_entities)
 
     return word_idfs
 
-def query_attention(word, query):
+def query_attention(word, query, named_entity):
     """
     Calculate the attention score for a word based on its relevance to the query.
 
@@ -171,6 +190,9 @@ def query_attention(word, query):
     # Check for exact match with query
     if word in query:
         return 1.0
+
+    if word in named_entity: 
+        return 0.8
 
     # Check for partial match with query
     for query_word in query:
@@ -203,13 +225,13 @@ def top_files(query, files, idfs, n):
               file_scores[filename] += tf_idf
 
     sorted_files = sorted([filename for filename in files], key = lambda x : file_scores[x], reverse=True)
-    print("Sorted Files:", sorted_files)
+    """ print("Sorted Files:", sorted_files) """
 
     # Return best n files
     return sorted_files[:n]
 
 
-def top_sentences(query, sentences, idfs, n):
+def top_sentences(query, sentences, idfs, named_entity, n):
     """
     Given a `query` (a set of words), `sentences` (a dictionary mapping
     sentences to a list of their words), and `idfs` (a dictionary mapping words
@@ -231,7 +253,7 @@ def top_sentences(query, sentences, idfs, n):
 
         # Calculate query attention score for the sentence
         for word in query:
-            s['query_attention_score'] += query_attention(word, query)
+            s['query_attention_score'] += query_attention(word, query, named_entity)
 
     # Rank sentences by combined score (idf + query attention) and return n sentences
     sorted_sentences = sorted([sentence for sentence in sentences], key=lambda x: (sentence_score[x]['idf_score'] + sentence_score[x]['query_attention_score']), reverse=True)

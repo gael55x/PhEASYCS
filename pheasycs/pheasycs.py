82826f6d203e077cf8288f76ff2e0dc6189e2dc2
GAILLELORD <gailleamolong5@gmail.com>
2023-09-27 08:07:23 +0400

    Update pheasycs.py

--- pheasycs/pheasycs.py
+++ pheasycs/pheasycs.py
@@ -1,18 +1,19 @@
+import sys
+import time
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 import numpy as np
-import tflearn
 import tensorflow as tf
 import json
-import pickle
 import random
 
 nltk.download("punkt")
 
 # Function to load intents data
 def load_intents_data(filename='intents.json'):
-    with open(filename) as intents_file:
+    with open(filename, 'r', encoding='utf-8') as intents_file:
         return json.load(intents_file)
+
 
 # Function to preprocess data
 def preprocess_data(data):
@@ -27,10 +28,15 @@ def preprocess_data(data):
             wrds = nltk.word_tokenize(pattern)
             words.extend(wrds)
             x_docs.append(wrds)
-            y_docs.append(intent['tag'])
+            # Use the get method to provide a default value ('unknown') when 'tag' is missing
+            if 'tag' in intent:
+                y_docs.append(intent['tag'])
+            else:
+                y_docs.append('unknown')
 
-            if intent['tag'] not in labels:
+            if 'tag' in intent and intent['tag'] not in labels:
                 labels.append(intent['tag'])
+
 
     words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
     words = sorted(list(set(words)))
@@ -58,20 +64,22 @@ def preprocess_data(data):
 
     return words, labels, np.array(training), np.array(output)
 
-# Function to build and train the model
-def build_and_train_model(training, output, model_filename='model.tflearn'):
-    net = tflearn.input_data(shape=[None, len(training[0])])
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
-    net = tflearn.regression(net)
 
-    model = tflearn.DNN(net)
+# Function to build and train the model using tf.keras
+def build_and_train_model(training, output, model_filename='model.h5'):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(len(training[0]),)),
+        tf.keras.layers.Dense(8),
+        tf.keras.layers.Dense(8),
+        tf.keras.layers.Dense(len(output[0]), activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     try:
-        model.load(model_filename)
+        model = tf.keras.models.load_model(model_filename)
     except:
-        model.fit(training, output, n_epoch=100, batch_size=8, show_metric=True)
+        model.fit(training, output, epochs=100, batch_size=8)
         model.save(model_filename)
 
     return model
@@ -86,7 +94,18 @@ def print_with_appearance(text):
         time.sleep(0.03)
     print()  # Move to the next line after printing the complete text
 
-# Function to process user input and get responses
+def bag_of_words(s):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return np.array([bag])  # Wrap the bag in an additional array to match the expected shape
+
 def get_response(model, words, labels, data, user_input):
     stemmer = LancasterStemmer()
 
@@ -100,10 +119,10 @@ def get_response(model, words, labels, data, user_input):
                 if w == se:
                     bag[i] = 1
 
-        return np.array(bag)
+        return np.array([bag])  # Wrap the bag in an additional array to match the expected shape
 
     # Probability of correct response
-    results = model.predict([bag_of_words(user_input)])
+    results = model.predict(bag_of_words(user_input))  # Remove the extra list []
 
     # Picking the greatest number from probability
     results_index = np.argmax(results)
@@ -112,12 +131,17 @@ def get_response(model, words, labels, data, user_input):
 
     responses = []
     for intent in data['intents']:
-        if intent['tag'] == tag:
-            responses.extend(intent['responses'])
+        if 'tag' in intent and intent['tag'] == tag:
+            if 'responses' in intent:  # Check if 'responses' exists in the intent
+                responses.extend(intent['responses'])
 
     # Print the response with a typing effect
-    for response in responses:
-        print_with_appearance("PhEASYCS: " + response)
+    if responses:
+        for response in responses:
+            print_with_appearance("PhEASYCS: " + response)
+    else:
+        print_with_appearance("PhEASYCS: I'm sorry, but I don't have a response for that question. As an AI language model, I am limited by the data sets provided, which is from DEPED Physics Modules.")
+
 
 if __name__ == "__main__":
     intents_data = load_intents_data()
